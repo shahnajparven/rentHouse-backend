48e953964a86a7781621172b41e09ebed7608d5a
shahnaj parven <shahnajritu21@gmail.com>
2024-03-14 11:49:11 +0600

    change

--- config/database.js
+++ config/database.js
@@ -2,14 +2,15 @@ import mongoose from "mongoose";
 
 const connectDatabase = () => {
   mongoose
-    .connect(process.env.DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      
-    })
+    .connect(process.env.DB_URI
+      // , {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,}
+    )
     .then((data) => {
       console.log(`Mongodb connected with server: ${data.connection.host}`);
     });
 };
+
 
 export default connectDatabase;
